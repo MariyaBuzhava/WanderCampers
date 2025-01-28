58e384dd4132c3932571280062535ae97e6c3748
Mariya Buzhava <bujava.masha@gmail.com>
2025-01-28 20:19:20 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/catalog" element={<Catalog />} />
+      <Route path="/catalog/:id" element={<Camper />}>
+        <Route path="/features" element={<Features />} />
+        <Route path="/reviews" element={<Reviews />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
