63afecad37b8276df9fb3d77f237666dbcb15344
Mariya Buzhava <bujava.masha@gmail.com>
2025-01-28 23:42:16 +0200

    update logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,7 @@ import Navigation from "../Navigation/Navigation";
 function Header() {
   return (
     <div className={c.container}>
-      <img
-        src="/assets/images/TravelTrucks.svg"
-        alt="Logo"
-        width="136"
-        height="16"
-      />
+      <img src="/images/TravelTrucks.svg" alt="Logo" width="136" height="16" />
       <Navigation />
     </div>
   );
