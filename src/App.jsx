e8d2145f234813cdba88b46934fd745d64050eb3
Mariya Buzhava <bujava.masha@gmail.com>
2025-01-28 21:20:32 +0200

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+import CatalogPage from "./pages/CatalogPage/CatalogPage";
+import CamperPage from "./pages/CamperPage/CamperPage";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/catalog" element={<Catalog />} />
-      <Route path="/catalog/:id" element={<Camper />}>
-        <Route path="/features" element={<Features />} />
-        <Route path="/reviews" element={<Reviews />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/catalog" element={<CatalogPage />} />
+      <Route path="/catalog/:id" element={<CamperPage />}>
+        {/* <Route path="/features" element={<Features />} />
+        <Route path="/reviews" element={<Reviews />} /> */}
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
