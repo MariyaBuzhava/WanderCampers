c9fae22a8c295d7bc72e7183497e8320191f1e36
Mariya Buzhava <bujava.masha@gmail.com>
2025-01-28 23:30:45 +0200

    add HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,17 @@
+import c from "./HomePage.module.css";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className={c.container}>
+      <div className={c.title}>
+        <h1 className={c.text1}>Campers of your dreams</h1>
+        <h3 className={c.text2}>
+          You can find everything you want in our catalog
+        </h3>
+        <button>View Now</button>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
