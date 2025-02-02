8d028495e7500adfdb836383ef893ff625cc926c
Mariya Buzhava <bujava.masha@gmail.com>
2025-02-02 20:29:20 +0200

    add Reviews

--- src/pages/CamperPage/CamperPage.jsx
+++ src/pages/CamperPage/CamperPage.jsx
@@ -10,6 +10,7 @@ import {
 } from "../../redux/selectors/campersSelectors";
 import BookingForm from "../../components/BookingForm/BookingForm";
 import Features from "../../components/Features/Features";
+import Reviews from "../../components/Reviews/Reviews";
 
 function CamperPage() {
   const { id } = useParams();
@@ -27,24 +28,6 @@ function CamperPage() {
 
   const handleTabClick = (section) => {
     setActiveSection(section);
-  };
-
-  const renderStars = (rating) => {
-    const totalStars = 5;
-    let stars = [];
-    for (let i = 0; i < totalStars; i++) {
-      stars.push(
-        <svg
-          key={i}
-          className={i < rating ? c.starActive : c.starInactive}
-          width="16"
-          height="16"
-        >
-          <use href="/images/symbol-defs.svg#icon-star" />
-        </svg>
-      );
-    }
-    return stars;
   };
 
   if (isLoading) return <p>Loading...</p>;
@@ -122,34 +105,7 @@ function CamperPage() {
       </div>
       <div className={c.content}>
         {activeSection === "features" && <Features />}
-        {activeSection === "reviews" && (
-          <div>
-            <div className={c.reviews}>
-              {camper.reviews && camper.reviews.length > 0 ? (
-                camper.reviews.map((review, index) => (
-                  <div key={index} className={c.review}>
-                    <div className={c.user}>
-                      <div className={c.avatar}>
-                        <span className={c.avatarLetter}>
-                          {review.reviewer_name[0].toUpperCase()}
-                        </span>
-                      </div>
-                      <div className={c.userInfo}>
-                        <p className={c.userName}>{review.reviewer_name}</p>
-                        <div className={c.rating}>
-                          {renderStars(review.reviewer_rating)}
-                        </div>
-                      </div>
-                    </div>
-                    <p className={c.comment}>{review.comment}</p>
-                  </div>
-                ))
-              ) : (
-                <p>No reviews yet</p>
-              )}
-            </div>
-          </div>
-        )}
+        {activeSection === "reviews" && <Reviews />}
         <BookingForm />
       </div>
     </div>
