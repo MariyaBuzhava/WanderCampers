2df92c9dcde9f35da5f320068dc01967ff731e15
Mariya Buzhava <bujava.masha@gmail.com>
2025-02-02 08:03:17 +0200

    add reviews

--- src/pages/CamperPage/CamperPage.jsx
+++ src/pages/CamperPage/CamperPage.jsx
@@ -27,6 +27,24 @@ function CamperPage() {
     setActiveSection(section);
   };
 
+  const renderStars = (rating) => {
+    const totalStars = 5;
+    let stars = [];
+    for (let i = 0; i < totalStars; i++) {
+      stars.push(
+        <svg
+          key={i}
+          className={i < rating ? c.starActive : c.starInactive}
+          width="16"
+          height="16"
+        >
+          <use href="/images/symbol-defs.svg#icon-star" />
+        </svg>
+      );
+    }
+    return stars;
+  };
+
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
   if (!camper) return <p>Camper not found</p>;
@@ -197,8 +215,30 @@ function CamperPage() {
         )}
         {activeSection === "reviews" && (
           <div>
-            <h3>Reviews Section</h3>
-            <p>Here are the reviews for the camper...</p>
+            <div className={c.reviews}>
+              {camper.reviews && camper.reviews.length > 0 ? (
+                camper.reviews.map((review, index) => (
+                  <div key={index} className={c.review}>
+                    <div className={c.user}>
+                      <div className={c.avatar}>
+                        <span className={c.avatarLetter}>
+                          {review.reviewer_name[0].toUpperCase()}
+                        </span>
+                      </div>
+                      <div className={c.userInfo}>
+                        <p className={c.userName}>{review.reviewer_name}</p>
+                        <div className={c.rating}>
+                          {renderStars(review.reviewer_rating)}
+                        </div>
+                      </div>
+                    </div>
+                    <p className={c.comment}>{review.comment}</p>
+                  </div>
+                ))
+              ) : (
+                <p>No reviews yet</p>
+              )}
+            </div>
           </div>
         )}
       </div>
